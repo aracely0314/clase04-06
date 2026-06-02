b311b42e3d432cbe1494ae63caf13b8d52b07a56
AracelyGonzález <ara.gonzalezv@duocuc.cl>
2026-06-02 14:10:26 -0400

    Agrega cálculos y validaciones

--- ejercicio_calculadora.py
+++ ejercicio_calculadora.py
@@ -1,5 +1,9 @@
 print("CALCULADORA DE DESCUENTO")
 nombre=input("Ingrese su nombre:")
 precio=int(input("Ingrese el precio:"))
+if precio<0:
+    print("ERROR! no puede ser un precio menor que 0")
 porcentaje_descuento=int(input("Ingrese el porcentaje de descuento:"))
-
+if porcentaje_descuento<0:
+    print("ERROR! no puede ser un descuento menor que 0")
+precio_final=precio-(precio*porcentaje_descuento/100)
