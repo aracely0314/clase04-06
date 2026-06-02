4befb1a01f874092f2c0d1e45d8f3a037b058114
AracelyGonzález <ara.gonzalezv@duocuc.cl>
2026-06-02 14:02:43 -0400

    Implementa ingreso de datos

--- ejercicio_calculadora.py
+++ ejercicio_calculadora.py
@@ -1,1 +1,5 @@
 print("CALCULADORA DE DESCUENTO")
+nombre=input("Ingrese su nombre:")
+precio=int(input("Ingrese el precio:"))
+porcentaje_descuento=int(input("Ingrese el porcentaje de descuento:"))
+
