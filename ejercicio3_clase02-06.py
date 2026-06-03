3f5924551697465f76b19e86b51bde9c480fb9d5
aracelygonzález <ara.gonzalezv@duocuc.cl>
2026-06-03 18:41:32 -0400

    Implementa ingreso de dato y ciclos

--- ejercicio3_clase02-06.py
+++ ejercicio3_clase02-06.py
@@ -1,7 +1,22 @@
-saldo_inicial=100000
+saldo=100000
 menu="""MENÚ:
 1. Consultar saldo
 2. Depositar dinero
 3. Girar dinero
 4. Salir"""
-print(menu)
+while True:
+    print(menu)
+    opcion=input("Seleccione una opción (1-4): ")
+    if opcion=="1":
+        print(f"Saldo actual: ${saldo}")
+    elif opcion=="2":
+        while True:
+                deposito=int(input("Monto a depositar: "))
+                break
+    elif opcion=="3":
+        while True:
+                giro=int(input("Monto a girar: "))
+                break
+    elif opcion=="4":
+        print("Gracias por usar el sistema. ¡Hasta luego!")
+        break
