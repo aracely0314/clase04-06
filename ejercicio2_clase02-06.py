6444615da75c44e7e90e48c0b42b426080789401
aracelygonzález <ara.gonzalezv@duocuc.cl>
2026-06-03 18:05:24 -0400

    Incorpora manejo de excepciones

--- ejercicio2_clase02-06.py
+++ ejercicio2_clase02-06.py
@@ -1,16 +1,28 @@
 print("PROMEDIO NOTAS")
-cantidad_notas = int(input("Cuántas notas deseas ingresar?: "))
-if cantidad_notas <= 0:
-    print("La cantidad de notas debe ser mayor a cero.")
+while True:
+    try:
+        cantidad_notas = int(input("Cuántas notas deseas ingresar?: "))
+        if cantidad_notas <= 0:
+            print("La cantidad de notas debe ser mayor a cero.")
+        else:
+            break
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número entero válido.")
+
+suma_notas = 0
 for i in range(cantidad_notas):
-    nota=float(input(f"Ingrese nota {i+1}: "))
-    if nota<0 or nota>10:
-        print("La nota debe ser mayor que 0 y menor o igual que 7.0")
-    else:
-       suma_notas += nota
+    while True:
+        try:
+            nota=float(input(f"Ingrese nota {i+1}: "))
+            if nota<0 or nota>10:
+                print("La nota debe ser mayor que 0 y menor o igual que 7.0")
+            else:
+                suma_notas += nota
+                break
+        except ValueError:
+            print("Entrada no válida. Por favor, ingrese un número decimal válido.")
 
 promedio = suma_notas / cantidad_notas
-
 if promedio>= 4.0:
     estado= "Aprobado"
 else:
