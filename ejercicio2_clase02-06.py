2d84b36ac2d7c9931de08d1369de8fec4cfa5285
aracelygonzález <ara.gonzalezv@duocuc.cl>
2026-06-03 18:00:03 -0400

    Agrega cálculos y validaciones

--- ejercicio2_clase02-06.py
+++ ejercicio2_clase02-06.py
@@ -1,3 +1,12 @@
 print("PROMEDIO NOTAS")
 cantidad_notas = int(input("Cuántas notas deseas ingresar?: "))
-nota=int(input("Ingrese nota: "))
+if cantidad_notas <= 0:
+    print("La cantidad de notas debe ser mayor a cero.")
+for i in range(cantidad_notas):
+    nota=float(input(f"Ingrese nota {i+1}: "))
+    if nota<0 or nota>10:
+        print("La nota debe ser mayor que 0 y menor o igual que 7.0")
+    else:
+       suma_notas += nota
+
+promedio = suma_notas / cantidad_notas
