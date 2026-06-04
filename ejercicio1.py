3aa0bb49193fbfb3512aa66987d633024b692d30
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-04 13:24:20 -0400

    Tercer commit, ejercicio 1 de prueba 3 con validaciones de excepciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,28 @@
 contador_ing_senior=0
 contador_ing_junior=0
-cantidad_ingenieros=int(input("Ingrese cantidad de ingenieros: "))
+while True:
+    try:
+        cantidad_ingenieros=int(input("Ingrese cantidad de ingenieros: "))
+        if cantidad_ingenieros>0:
+            break
+        print("¡Dato inválido! Ingresa un entero positivo para continuar el registro")
+    except ValueError:
+        print("¡Dato inválido! Ingresa un entero positivo para continuar el registro")
 
 for i in range(cantidad_ingenieros):
-    nombre=input("Ingrese nombre del ingeniero: ")
-    nivel_tecnico=int(input("Ingrese mivel del ingeniero: "))
+    while True:
+        nombre=input("Ingrese nombre del ingeniero: ").strip()
+        if len(nombre)>=6 and " " not in nombre:
+            break
+        print("Error! el nombre debe tener al menos 6 caracteres y no contener espacios")
+    while True:
+        try:
+            nivel_tecnico=int(input("Ingrese mivel del ingeniero: "))
+            if nivel_tecnico>0:
+                break
+            print("¡Error de validación! Ingresa un número entero positivo para el nivel técnico.")
+        except ValueError:
+            print("¡Error de validación! Ingresa un número entero positivo para el nivel técnico.")
     if nivel_tecnico>45:
         contador_ing_senior+=1
     else: 
