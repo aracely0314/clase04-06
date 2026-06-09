cb8919b20b390924c1e51ffa0d0c8f94a4c609d6
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-08 23:16:03 -0400

    Segundo commit, segunda opción del menú con validaciones y excepcion de errores agregadas, contadores creados

--- ejercicio2prueba3.py
+++ ejercicio2prueba3.py
@@ -7,6 +7,12 @@ menu_principal="""===MENÚ PRINCIPAL===
 4. Historial de ocupaciones
 5. Salir"""
 habitaciones_disponibles=50
+reservar_habitacion=0
+habitaciones_disponibles=50
+total_reservas=0
+cancelar_habitacion=0
+habitaciones_en_reserva=0
+total_habitaciones_reservadas=0
 while True:
     os.system("cls")
     print(menu_principal)
@@ -18,7 +24,30 @@ while True:
         print("Volviendo al menú principal...")
         time.sleep(2)
     elif opcion=="2":
-        pass
+        print("REALIZAR CHECK-IN")
+        while True:
+            if habitaciones_disponibles==0:
+                print("No hay habitaciones disponibles")
+                print("Volviendo al menú principal...")
+                time.sleep(1.3)
+                break
+            try:
+                reservar_habitacion=int(input("Ingrese cantidad de habitaciones que desea reservar:"))
+                if reservar_habitacion>0 and reservar_habitacion<=habitaciones_disponibles:
+                    print(f"{reservar_habitacion} habitaciones reservadas con éxito!")
+                    habitaciones_disponibles-=reservar_habitacion
+                    habitaciones_en_reserva+=reservar_habitacion
+                    total_reservas+=1
+                    total_habitaciones_reservadas+=reservar_habitacion
+                    print("Volviendo al menú principal...")
+                    time.sleep(1.5)
+                    break
+                elif reservar_habitacion>habitaciones_disponibles:
+                    print("Cantidad de habitaciones que desea reservar supera la cantidad de habitaciones disponibles.")
+                    continue
+                print ("Error! Debe ingresar un número entero positivo válido")
+            except ValueError:
+                print("Error! Debe ingresar un número entero positivo válido")
     elif opcion=="3":
         pass
     elif opcion=="4":
