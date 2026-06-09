b43d447c980da042cbacf976ac6f195c8f81fddb
aracely0314 <ara.gonzalezv@duocuc.cl>
2026-06-08 23:35:22 -0400

    Segundo commit, segunda opcion agregada con excepcion de errores y condiciones, contadores creados

--- ejercicio2prueba3.py
+++ ejercicio2prueba3.py
@@ -6,6 +6,11 @@ menu_principal= """*** MENU PRINCIPAL ***
 4.- Historial de reservas.
 5.- Salir."""
 cupos_disponibles=75
+reservar_cupos=0
+total_reservas=0
+cancelar_cupos=0
+cupos_en_reserva=0
+total_cupos_reservados=0
 print("¡Bienvenido al sistema de gestión de cupos del Gimnasio Titan!")
 while True:
     os.system("cls")
@@ -18,7 +23,30 @@ while True:
         print("Volviendo al menú principal...")
         time.sleep(2)
     elif opcion=="2":
-       pass
+        print("REALIZAR CHECK-IN")
+        while True:
+            if cupos_disponibles==0:
+                print("No hay cupos disponibles")
+                print("Volviendo al menú principal...")
+                time.sleep(1.3)
+                break
+            try:
+                reservar_cupos=int(input("Ingrese cantidad de cupos que desea reservar:"))
+                if reservar_cupos>0 and reservar_cupos<=cupos_disponibles:
+                    print(f"{reservar_cupos} cupos reservados con éxito!")
+                    cupos_disponibles-=reservar_cupos
+                    cupos_en_reserva+=reservar_cupos
+                    total_reservas+=1
+                    total_cupos_reservados+=reservar_cupos
+                    print("Volviendo al menú principal...")
+                    time.sleep(1.5)
+                    break
+                elif reservar_cupos>cupos_disponibles:
+                    print("Cantidad de cupos que desea reservar supera la cantidad de cupos disponibles.")
+                    continue
+                print ("Error! Debe ingresar un número entero positivo válido")
+            except ValueError:
+                print("Error! Debe ingresar un número entero positivo válido")
     elif opcion=="3":
         pass
     elif opcion=="4":
